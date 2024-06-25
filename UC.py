1d944659c4b1157d94a660e9e07fdc3d3a0235b5
hzhacom <78690860@qq.com>
2024-06-25 12:16:47 +0800

    useful code

---
+++ UC.py
@@ -0,0 +1,16 @@
+"""
+有用的代码
+"""
+
+
+
+
+
+
+
+
+# pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pyside6
+# https://www.pythonguis.com/tutorials/pyqt6-widgets/
+# sfc /scannow
+# ^\s*(?=\r?$)\n
+# ipconfig /all
